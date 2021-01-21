f03e07c9c11712aba219c4d6964587839f51d5ab
Polly <71594933+PollyDimieva@users.noreply.github.com>
2021-01-21 16:09:18 +0200

    Update ReadingWindow.java

--- Step2 - Initial GUI Implementation/ReadingWindow.java
+++ Step2 - Initial GUI Implementation/ReadingWindow.java
@@ -29,45 +29,15 @@ public class ReadingWindow {
 	    JButton fictionButton = new JButton("...fiction?");
 	    fictionButton.setBounds(145, 100, 110, 30);
 	    frame.getContentPane().add(fictionButton);
-	    fictionButton.addActionListener(new ActionListener(){  
-	    	public void actionPerformed(ActionEvent e){  
-	            frame.setVisible(false);
-	            FictionWindow fictionWindow = new FictionWindow();
-	            FictionWindow.showWindow();
-	        }  
-	    }); 
+	
 	       
 	    JButton nonfictionButton = new JButton("...nonfiction?");
 	    nonfictionButton.setBounds(145, 200,110, 30);
 	    frame.getContentPane().add(nonfictionButton);
-	    nonfictionButton.addActionListener(new ActionListener(){  
-	    	public void actionPerformed(ActionEvent e){  
-	            frame.setVisible(false);
-	            NonfictionWindow nonfictionWindow = new NonfictionWindow();
-	            nonfictionWindow.showWindow();
-	        }  
-	    }); 
-	    
-	    
-	    
 	    
 	    JButton back = new JButton("Go back");
 	    back.setBounds(10, 400, 80, 30);
 	    frame.getContentPane().add(back);
-	    back.addActionListener(new ActionListener(){  
-	    	public void actionPerformed(ActionEvent e){  
-	            frame.setVisible(false);
-	            MainWindow mainWindow = new MainWindow();
-	            mainWindow.showWindow();
-	        }  
-	    }); 
-
-	    
-	    
-	    
-	    
-	    
-	    
 	    
 	    
 	    frame.setVisible(true);
